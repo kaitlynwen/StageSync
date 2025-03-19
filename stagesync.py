e2941035c3d39e094d45e2c0231d264dabfa49cc
Kaitlyn H. Wen <kaitlynwen@princeton.edu>
2025-03-18 23:05:36 -0400

    add more routes

--- stagesync.py
+++ stagesync.py
@@ -46,6 +46,108 @@ def home():
         # Render home.html for regular users
         return render_template('home.html', user=user_info)
 
+@app.route('/settings', methods=['GET'])
+def settings():
+    user_info = {
+        'user': 'Admin User',
+        'is_admin': True
+    }
+    return render_template('settings.html', user=user_info)
+
+@app.route('/update-availability', methods=['GET'])
+def update():
+    user_info = {
+        'user': 'Admin User',
+        'is_admin': True
+    }
+    return render_template('update.html', user=user_info)
+
+@app.route('/view-schedule', methods=['GET'])
+def view():
+    user_info = {
+        'user': 'Admin User',
+        'is_admin': True
+    }
+    return render_template('view.html', user=user_info)
+
+@app.route('/upload-data', methods=['GET'])
+def upload():
+    user_info = {
+        'user': 'Admin User',
+        'is_admin': True
+    }
+        # Check if the user is an admin
+    if user_info.get('is_admin', False):
+        return render_template('upload.html', user=user_info)
+    else:
+        return None
+
+@app.route('/generate-schedule', methods=['GET'])
+def generate():
+    user_info = {
+        'user': 'Admin User',
+        'is_admin': True
+    }
+    if user_info.get('is_admin', False):
+        return render_template('generate.html', user=user_info)
+    else:
+        return None
+
+@app.route('/publish-schedule', methods=['GET'])
+def publish():
+    user_info = {
+        'user': 'Admin User',
+        'is_admin': True
+    }
+    if user_info.get('is_admin', False):
+        return render_template('publish.html', user=user_info)
+    else:
+        return None
+    
+@app.route('/manage-members', methods=['GET'])
+def manage_admins():
+    user_info = {
+        'user': 'Admin User',
+        'is_admin': True
+    }
+    if user_info.get('is_admin', False):
+        return render_template('manage-members.html', user=user_info)
+    else:
+        return None
+
+@app.route('/manage-admins', methods=['GET'])
+def manage_admins():
+    user_info = {
+        'user': 'Admin User',
+        'is_admin': True
+    }
+    if user_info.get('is_admin', False):
+        return render_template('manage-admins.html', user=user_info)
+    else:
+        return None
+    
+@app.route('/manage-groups', methods=['GET'])
+def manage_groups():
+    user_info = {
+        'user': 'Admin User',
+        'is_admin': True
+    }
+    if user_info.get('is_admin', False):
+        return render_template('groups.html', user=user_info)
+    else:
+        return None
+    
+@app.route('/availability', methods=['GET'])
+def availability():
+    user_info = {
+        'user': 'Admin User',
+        'is_admin': True
+    }
+    if user_info.get('is_admin', False):
+        return render_template('availability.html', user=user_info)
+    else:
+        return None
+
 #-----------------------------------------------------------------------
 
 # If the file is being executed directly, run the app
