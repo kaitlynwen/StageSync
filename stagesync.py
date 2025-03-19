c87316d40c3d007e8a1956961a2369cc8e121b03
Kaitlyn H. Wen <kaitlynwen@princeton.edu>
2025-03-18 23:28:08 -0400

    change routes for flask

--- stagesync.py
+++ stagesync.py
@@ -5,7 +5,7 @@
 # Author: Kaitlyn Wen, Michael Igbinoba, Timothy Sim
 #-----------------------------------------------------------------------
 
-from flask import render_template
+from flask import render_template, redirect, url_for
 import flask
 import os
 import dotenv
@@ -26,127 +26,97 @@ app = flask.Flask(
 # Secret key setup (set up in .env)
 app.secret_key = os.environ.get('APP_SECRET_KEY', 'your_default_secret_key')  # Make sure .env has the APP_SECRET_KEY
 
+#-----------------------------------------------------------------------s
+
+# Define user info function (currently hardcoded for bypassing authentication)
+def get_user_info():
+    return {
+        'user': 'Admin User',
+        'is_admin': True
+    }
+    
 #-----------------------------------------------------------------------
 
 # Routes
 @app.route('/', methods=['GET'])
 @app.route('/home', methods=['GET'])
 def home():
-    # Temporarily bypass authentication and default to admin
-    user_info = {
-        'user': 'Admin User',
-        'is_admin': True
-    }
-    
-    # Check if the user is an admin
+    user_info = get_user_info()
     if user_info.get('is_admin', False):
-        # Render home-admin.html if the user is an admin
         return render_template('home-admin.html', user=user_info)
     else:
-        # Render home.html for regular users
         return render_template('home.html', user=user_info)
 
 @app.route('/settings', methods=['GET'])
 def settings():
-    user_info = {
-        'user': 'Admin User',
-        'is_admin': True
-    }
+    user_info = get_user_info()
     return render_template('settings.html', user=user_info)
 
 @app.route('/update-availability', methods=['GET'])
 def update():
-    user_info = {
-        'user': 'Admin User',
-        'is_admin': True
-    }
+    user_info = get_user_info()
     return render_template('update.html', user=user_info)
 
 @app.route('/view-schedule', methods=['GET'])
 def view():
-    user_info = {
-        'user': 'Admin User',
-        'is_admin': True
-    }
+    user_info = get_user_info()
     return render_template('view.html', user=user_info)
 
 @app.route('/upload-data', methods=['GET'])
 def upload():
-    user_info = {
-        'user': 'Admin User',
-        'is_admin': True
-    }
-        # Check if the user is an admin
+    user_info = get_user_info()
     if user_info.get('is_admin', False):
         return render_template('upload.html', user=user_info)
     else:
-        return None
+        return redirect(url_for('home'))
 
 @app.route('/generate-schedule', methods=['GET'])
 def generate():
-    user_info = {
-        'user': 'Admin User',
-        'is_admin': True
-    }
+    user_info = get_user_info()
     if user_info.get('is_admin', False):
         return render_template('generate.html', user=user_info)
     else:
-        return None
+        return redirect(url_for('home'))
 
 @app.route('/publish-schedule', methods=['GET'])
 def publish():
-    user_info = {
-        'user': 'Admin User',
-        'is_admin': True
-    }
+    user_info = get_user_info()
     if user_info.get('is_admin', False):
         return render_template('publish.html', user=user_info)
     else:
-        return None
+        return redirect(url_for('home'))
     
 @app.route('/manage-members', methods=['GET'])
-def manage_admins():
-    user_info = {
-        'user': 'Admin User',
-        'is_admin': True
-    }
+def manage_members():
+    user_info = get_user_info()
     if user_info.get('is_admin', False):
         return render_template('manage-members.html', user=user_info)
     else:
-        return None
+        return redirect(url_for('home'))
 
-@app.route('/manage-admins', methods=['GET'])
-def manage_admins():
-    user_info = {
-        'user': 'Admin User',
-        'is_admin': True
-    }
+@app.route('/manage-users', methods=['GET'])
+def manage_users():
+    user_info = get_user_info()
     if user_info.get('is_admin', False):
-        return render_template('manage-admins.html', user=user_info)
+        return render_template('manage-users.html', user=user_info)
     else:
-        return None
+        return redirect(url_for('home'))
     
 @app.route('/manage-groups', methods=['GET'])
 def manage_groups():
-    user_info = {
-        'user': 'Admin User',
-        'is_admin': True
-    }
+    user_info = get_user_info()
     if user_info.get('is_admin', False):
         return render_template('groups.html', user=user_info)
     else:
-        return None
+        return redirect(url_for('home'))
     
 @app.route('/availability', methods=['GET'])
 def availability():
-    user_info = {
-        'user': 'Admin User',
-        'is_admin': True
-    }
+    user_info = get_user_info()
     if user_info.get('is_admin', False):
         return render_template('availability.html', user=user_info)
     else:
-        return None
+        return redirect(url_for('home'))
 
 #-----------------------------------------------------------------------
 
