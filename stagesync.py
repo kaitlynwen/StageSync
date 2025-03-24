b9b092fe136dbbd76eecbc33d72d79b8b2b47f5f
Kaitlyn H. Wen <kaitlynwen@princeton.edu>
2025-03-24 05:00:34 -0400

    working view member availability

--- stagesync.py
+++ stagesync.py
@@ -57,16 +57,16 @@ def allowed_file(filename):
 # Define user info function (currently hardcoded for bypassing authentication)
 def get_user_info():
     user_info = auth.authenticate()
-    netid = user_info['user']
-    is_admin = False #Default false
+    netid = user_info["user"]
+    is_admin = False  # Default false
 
     # Based on PostgreSQL/authorsearch.py
     try:
         with psycopg2.connect(DATABASE_URL) as conn:
 
             with conn.cursor() as cur:
-                query = 'SELECT is_admin FROM users '
-                query += 'WHERE netid = \'' + netid + '\''
+                query = "SELECT is_admin FROM users "
+                query += "WHERE netid = '" + netid + "'"
                 cur.execute(query)
 
                 row = cur.fetchone()
@@ -74,7 +74,7 @@ def get_user_info():
                     is_admin = row[0]
 
     except Exception as ex:
-        pass # for now
+        pass  # for now
 
     return {"user": netid, "is_admin": is_admin}
 
@@ -82,16 +82,72 @@ def get_user_info():
 # -----------------------------------------------------------------------
 
 
+def get_all_users():
+    """Fetches all members from the database to populate the dropdown."""
+    try:
+        with psycopg2.connect(DATABASE_URL) as conn:
+            with conn.cursor() as cur:
+                cur.execute(
+                    "SELECT netid, first_name, last_name FROM users ORDER BY last_name, first_name"
+                )
+                members = cur.fetchall()
+
+        # Convert result to a list of dictionaries
+        return [
+            {"netid": row[0], "first_name": row[1], "last_name": row[2]}
+            for row in members
+        ]
+
+    except Exception as e:
+        print(f"Error fetching members: {e}")
+        return []  # Return empty list in case of failure
+
+
+# -----------------------------------------------------------------------
+
+
+def get_user_by_netid(netid):
+    """Fetches a user from the database by netid."""
+    try:
+        with psycopg2.connect(DATABASE_URL) as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    """
+                    SELECT first_name, last_name FROM users
+                    WHERE netid = %s
+                """,
+                    (netid,),
+                )
+                member = cursor.fetchone()
+                
+        return {"first_name": member[0], "last_name": member[1]}
+
+    except Exception as e:
+        print(netid)
+        print(f"Error fetching members: {e}")
+        return []  # Return empty list in case of failure
+
+
+# -----------------------------------------------------------------------
+
 def get_admin_users():
     """Fetch all admin users from the database."""
     admin_users = []
     try:
         with psycopg2.connect(DATABASE_URL) as conn:
             with conn.cursor() as cur:
-                cur.execute("SELECT netid, first_name, last_name FROM users WHERE is_admin = TRUE")
+                cur.execute(
+                    "SELECT netid, first_name, last_name FROM users WHERE is_admin = TRUE"
+                )
                 for netid, first_name, last_name in cur.fetchall():
-                    admin_users.append({"netid":netid, "first_name": first_name, "last_name": last_name})
-                    
+                    admin_users.append(
+                        {
+                            "netid": netid,
+                            "first_name": first_name,
+                            "last_name": last_name,
+                        }
+                    )
+
     except Exception as ex:
         print("Database error:", ex)
 
@@ -121,8 +177,10 @@ def get_groups():
                 )
                 for group_name, first_name, last_name in cur.fetchall():
                     if group_name not in groups:
-                        groups[group_name] = [] 
-                    groups[group_name].append({"first_name": first_name, "last_name": last_name})
+                        groups[group_name] = []
+                    groups[group_name].append(
+                        {"first_name": first_name, "last_name": last_name}
+                    )
 
     except Exception as ex:
         print("Database error:", ex)
@@ -152,101 +210,121 @@ def settings():
 def update():
     if request.method == "GET":
         user_info = get_user_info()
-        user_netid = user_info['user']
+        user_netid = user_info["user"]
         weekly_conflicts = get_weekly_conflict(user_netid)
         one_time_conflicts, conflict_notes = get_one_time_conflict(user_netid)
-        return render_template("update.html", user=user_info, 
-                               weekly_conflicts=weekly_conflicts,
-                               one_time_conflicts=one_time_conflicts,
-                               conflict_notes=conflict_notes)
+        return render_template(
+            "update.html",
+            user=user_info,
+            weekly_conflicts=weekly_conflicts,
+            one_time_conflicts=one_time_conflicts,
+            conflict_notes=conflict_notes,
+        )
 
     else:
         user_info = get_user_info()  # Fetch user info (netid) for the current user
-        user_netid = user_info['user']  # Get the user netid
+        user_netid = user_info["user"]  # Get the user netid
         delete_conflict(user_netid)
         weekly_conflicts = {
-            "Monday": request.form['monday_conflicts'],
-            "Tuesday": request.form['tuesday_conflicts'],
-            "Wednesday": request.form['wednesday_conflicts'],
-            "Thursday": request.form['thursday_conflicts'],
-            "Friday": request.form['friday_conflicts'],
-            "Saturday": request.form['saturday_conflicts'],
-            "Sunday": request.form['sunday_conflicts'],
+            "Monday": request.form["monday_conflicts"],
+            "Tuesday": request.form["tuesday_conflicts"],
+            "Wednesday": request.form["wednesday_conflicts"],
+            "Thursday": request.form["thursday_conflicts"],
+            "Friday": request.form["friday_conflicts"],
+            "Saturday": request.form["saturday_conflicts"],
+            "Sunday": request.form["sunday_conflicts"],
         }
-        one_time_conflicts = request.form['one_time_conflict']
-        conflict_notes = request.form['conflict_notes']
-        
+        one_time_conflicts = request.form["one_time_conflict"]
+        conflict_notes = request.form["conflict_notes"]
+
         # Parse and insert weekly conflicts
         for day, conflicts in weekly_conflicts.items():
             if conflicts:
-                for conflict in conflicts.split(';'):
-                    start_time, end_time = conflict.split('-')
+                for conflict in conflicts.split(";"):
+                    start_time, end_time = conflict.split("-")
                     start_time = convert_to_24hr_format(start_time.strip())
                     end_time = convert_to_24hr_format(end_time.strip())
                     insert_weekly_conflict(user_netid, day, start_time, end_time)
 
         # Parse and insert one-time conflicts
         if one_time_conflicts:
-            one_time_list = one_time_conflicts.split(';')
+            one_time_list = one_time_conflicts.split(";")
             for conflict in one_time_list:
-                date_str, time_range = conflict.split('.')
+                date_str, time_range = conflict.split(".")
                 date_str = date_str.strip()
                 time_range = time_range.strip()
-                date = datetime.strptime(date_str, '%m/%d').replace(year=datetime.now().year)
-                day = date.strftime('%A')
-                start_time, end_time = time_range.split('-')
+                date = datetime.strptime(date_str, "%m/%d").replace(
+                    year=datetime.now().year
+                )
+                day = date.strftime("%A")
+                start_time, end_time = time_range.split("-")
                 start_time = convert_to_24hr_format(start_time.strip())
                 end_time = convert_to_24hr_format(end_time.strip())
-                insert_one_time_conflict(user_netid, date, day, start_time, end_time, conflict_notes)
+                insert_one_time_conflict(
+                    user_netid, date, day, start_time, end_time, conflict_notes
+                )
 
         weekly_conflicts = get_weekly_conflict(user_netid)
         one_time_conflicts, conflict_notes = get_one_time_conflict(user_netid)
         success_message = "Availability successfully updated!"
-        return render_template("update.html", user=user_info,
-                               success_message=success_message,
-                               weekly_conflicts=weekly_conflicts,
-                               one_time_conflicts=one_time_conflicts,
-                               conflict_notes=conflict_notes)
+        return render_template(
+            "update.html",
+            user=user_info,
+            success_message=success_message,
+            weekly_conflicts=weekly_conflicts,
+            one_time_conflicts=one_time_conflicts,
+            conflict_notes=conflict_notes,
+        )
+
 
 # Convert time to 24-hour format for PostgreSQL
 def convert_to_24hr_format(time_str):
-    return datetime.strptime(time_str, '%I:%M%p').strftime('%H:%M:%S')
+    return datetime.strptime(time_str, "%I:%M%p").strftime("%H:%M:%S")
+
 
 # Convert time to 12-hour format for html
 def convert_to_12hr_format(time_str):
-     return time_str.strftime("%I:%M %p").replace(' ', '')
+    return time_str.strftime("%I:%M %p").replace(" ", "")
+
 
 # Delete existing time conflicts from database
 def delete_conflict(netid):
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cursor:
-            cursor.execute("""
+            cursor.execute(
+                """
                 DELETE FROM availability
                 WHERE netid = %s
-            """, (netid,))
+            """,
+                (netid,),
+            )
             conn.commit()
+
 
 # Get existing weekly conflicts from database
 def get_weekly_conflict(netid):
     weekly_conflicts = {
-            "Monday": [],
-            "Tuesday": [],
-            "Wednesday": [],
-            "Thursday": [],
-            "Friday": [],
-            "Saturday": [],
-            "Sunday": [],
-        }
-    
+        "Monday": [],
+        "Tuesday": [],
+        "Wednesday": [],
+        "Thursday": [],
+        "Friday": [],
+        "Saturday": [],
+        "Sunday": [],
+    }
+
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cursor:
-            cursor.execute("""
+            cursor.execute(
+                """
                 SELECT day_of_week, start_time, end_time
                 FROM availability
                 WHERE netid = %s AND is_recurring = TRUE
                 ORDER BY day_of_week, start_time
-            """, (netid,))
-            
+            """,
+                (netid,),
+            )
+
             for day, start, end in cursor.fetchall():
                 start = convert_to_12hr_format(start)
                 end = convert_to_12hr_format(end)
@@ -254,64 +332,80 @@ def get_weekly_conflict(netid):
 
     return weekly_conflicts
 
+
 # Get existing one time conflicts from database
 def get_one_time_conflict(netid):
     one_time_conflicts = []
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cursor:
-            cursor.execute("""
+            cursor.execute(
+                """
                 SELECT one_time_date, start_time, end_time
                 FROM availability
                 WHERE netid = %s AND is_recurring = FALSE
                 ORDER BY one_time_date, start_time
-            """, (netid,))
-            
+            """,
+                (netid,),
+            )
+
             for date, start, end in cursor.fetchall():
-                date = date.strftime('%m/%d')
+                date = date.strftime("%m/%d")
                 start = convert_to_12hr_format(start)
                 end = convert_to_12hr_format(end)
                 one_time_conflicts.append(f"{date}.{start}-{end}")
-            
-            cursor.execute("""
+
+            cursor.execute(
+                """
                 SELECT notes
                 FROM availability
                 WHERE netid = %s AND is_recurring = FALSE
                 ORDER BY one_time_date, start_time
-            """, (netid,))
-            
+            """,
+                (netid,),
+            )
+
             row = cursor.fetchone()
             if row:
                 conflict_notes = row[0]
             else:
-                conflict_notes = ''
-    
+                conflict_notes = ""
+
     if not one_time_conflicts:
         return [], conflict_notes
 
     else:
         return one_time_conflicts, conflict_notes
 
+
 # Insert weekly conflict into the database
 def insert_weekly_conflict(netid, day, start_time, end_time):
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cursor:
-            cursor.execute("""
+            cursor.execute(
+                """
                 INSERT INTO availability (netid, day_of_week, start_time, 
                            end_time, is_recurring, one_time_date, notes)
                 VALUES (%s, %s, %s, %s, TRUE, NULL, NULL)
-            """, (netid, day, start_time, end_time))
+            """,
+                (netid, day, start_time, end_time),
+            )
             conn.commit()
+
 
 # Insert one-time conflict into the database
 def insert_one_time_conflict(netid, one_time_date, day, start_time, end_time, notes):
     with psycopg2.connect(DATABASE_URL) as conn:
         with conn.cursor() as cursor:
-            cursor.execute("""
+            cursor.execute(
+                """
                 INSERT INTO availability (netid, day_of_week, start_time, 
                            end_time, is_recurring, one_time_date, notes)
                 VALUES (%s, %s, %s, %s, FALSE, %s, %s)
-            """, (netid, day, start_time, end_time, one_time_date, notes))
+            """,
+                (netid, day, start_time, end_time, one_time_date, notes),
+            )
             conn.commit()
+
 
 @app.route("/view-schedule", methods=["GET"])
 def view():
@@ -348,7 +442,7 @@ def upload():
 @app.route("/generate-schedule", methods=["GET"])
 def generate():
     user_info = get_user_info()
-    if user_info.get("is_admin", False):
+    if user_info.get("is_admin", True):
         return render_template("generate.html", user=user_info)
     else:
         return redirect(url_for("home"))
@@ -357,7 +451,7 @@ def generate():
 @app.route("/published-schedule", methods=["GET"])
 def publish():
     user_info = get_user_info()
-    if user_info.get("is_admin", False):
+    if user_info.get("is_admin", True):
         return render_template("publish.html", user=user_info)
     else:
         return redirect(url_for("home"))
@@ -367,7 +461,7 @@ def publish():
 def manage_users():
     user_info = get_user_info()
     admin_info = get_admin_users()
-    if user_info.get("is_admin", False):
+    if user_info.get("is_admin", True):
         return render_template("manage-admins.html", user=user_info, admins=admin_info)
     else:
         return redirect(url_for("home"))
@@ -465,19 +559,49 @@ def remove_admins():
 def manage_groups():
     user_info = get_user_info()
     group_info = get_groups()
-    if user_info.get("is_admin", False):
+    if user_info.get("is_admin", True):
         return render_template("manage-groups.html", user=user_info, groups=group_info)
     else:
         return redirect(url_for("home"))
 
 
-@app.route("/view-availability", methods=["GET"])
+@app.route("/view-availability", methods=["GET", "POST"])
 def availability():
     user_info = get_user_info()
-    if user_info.get("is_admin", False):
-        return render_template("availability.html", user=user_info)
-    else:
-        return redirect(url_for("home"))
+
+    if user_info.get("is_admin", True):
+        members = get_all_users()  # Fetch members list
+
+        if request.method == "POST":
+            selected_netid = request.form.get("selected_netid")
+            if selected_netid:
+                selected_member = get_user_by_netid(selected_netid)
+                weekly_conflicts = get_weekly_conflict(selected_netid)
+                one_time_conflicts, conflict_notes = get_one_time_conflict(
+                    selected_netid
+                )
+
+                return render_template(
+                    "availability.html",
+                    user=user_info,
+                    members=members,
+                    selected_member=selected_member,
+                    weekly_conflicts=weekly_conflicts,
+                    one_time_conflicts=one_time_conflicts,
+                    conflict_notes=conflict_notes,
+                )
+
+        return render_template(
+            "availability.html",
+            user=user_info,
+            members=members,
+            selected_member=None,
+            weekly_conflicts=None,
+            one_time_conflicts=None,
+            conflict_notes=None,
+        )
+
+    return redirect(url_for("home"))
 
 
 # -----------------------------------------------------------------------
