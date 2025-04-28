8cbacca84eca9666559fca951e3c63a75a02d1bd
Timothy-Sim <ts2188@princeton.edu>
2025-04-28 15:04:05 -0400

    Update OIT API URL to fix members not being added

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ class Configs:
     def __init__(self):
         self.CONSUMER_KEY = "0JeE9Ekhps68U2_S8gk29oDgQlwa"
         self.CONSUMER_SECRET = "waORdCSITaX22tGftAIF20TjfVYa"
-        self.BASE_URL="https://api.princeton.edu:443/active-directory/1.0.5"
+        self.BASE_URL="https://api.princeton.edu:443/active-directory/1.0.6"
         self.USERS_BASIC = "/users/basic"
         self.REFRESH_TOKEN_URL="https://api.princeton.edu:443/token"
         self._refreshToken(grant_type="client_credentials")
