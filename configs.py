7a060c8452bd7e6375363e039f5f65625e4cc08b
Timothy-Sim <ts2188@princeton.edu>
2025-05-07 17:15:36 -0400

    env variable for ActiveDirectory API

--- configs.py
+++ configs.py
@@ -1,12 +1,18 @@
 import requests
 import json
 import base64
+import os
+from dotenv import load_dotenv
+
+# ----------------------------------------------------------------------
+
+load_dotenv()
 
 class Configs:
     def __init__(self):
         self.CONSUMER_KEY = "0JeE9Ekhps68U2_S8gk29oDgQlwa"
         self.CONSUMER_SECRET = "waORdCSITaX22tGftAIF20TjfVYa"
-        self.BASE_URL="https://api.princeton.edu:443/active-directory/1.0.6"
+        self.BASE_URL = os.getenv("BASE_URL", "https://api.princeton.edu:443/active-directory/1.0.6")
         self.USERS_BASIC = "/users/basic"
         self.REFRESH_TOKEN_URL="https://api.princeton.edu:443/token"
         self._refreshToken(grant_type="client_credentials")
